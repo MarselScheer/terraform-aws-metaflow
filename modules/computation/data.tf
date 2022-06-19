9848f6903a6b28405091c8e124d622f88838d04a
Marsel Scheer <scheer@freescience.de>
2022-06-19 10:26:39 +0200

    switch cpu-ami to gpu-version. unknown implication for rest of infrastruct

--- modules/computation/data.tf
+++ modules/computation/data.tf
@@ -1,5 +1,5 @@
 data "aws_region" "current" {}
 
 data "aws_ssm_parameter" "ecs_optimized_cpu_ami" {
-  name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended"
+  name = "/aws/service/ecs/optimized-ami/amazon-linux-2/gpu/recommended"
 }
